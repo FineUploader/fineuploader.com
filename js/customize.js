b1e4c2952feae381635ebf23444d7ecd473f3403
Ray Nicholus <rnicholus@users.noreply.github.com>
2018-03-08 23:58:13 -0600

    5.16.0

--- js/customize.js
+++ js/customize.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     var BASE_URL = 'https://github.com/FineUploader/fine-uploader/releases',
-        version = '5.15.7',
+        version = '5.16.0',
 
         updateDownloadUrl = function() {
             var base = 'fine-uploader.zip',
